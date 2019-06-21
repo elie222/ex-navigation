77845e70a0847f10abda4cd9985a1f67b91bbff9
Elie Steinbock <eliesteinbock@gmail.com>
2019-06-21 13:34:07 +0300

    fixed iphone x check

--- src/utils/isIPhoneX.js
+++ src/utils/isIPhoneX.js
@@ -1,4 +1,4 @@
 import { Platform, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
-const isIPhoneX = Platform.OS === 'ios' && width === 375 && height === 812;
+const isIPhoneX = Platform.OS === 'ios' && width >= 375 && height >= 812;
 export default isIPhoneX;
